745043f77b5578955bd63a24d2cde7892fdb530d
trevor.swain <trevor.swain@cgi.com>
2022-05-05 08:12:23 +0100

    NAL-103: Corrected build

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,12 @@
-import bootstrap from './bootstrap';
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import './services/i18n/i18n';
+import App from './components/App/App';
 
-bootstrap();
+ReactDOM.render(
+  <Suspense fallback="...is loading">
+    <App />
+  </Suspense>,
+  document.getElementById('root')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -20,13 +20,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /bootstrap\.jsx$/,
-        loader: 'bundle-loader',
-        options: {
-          lazy: true
-        }
-      },
-      {
         test: /\.m?js|\.jsx/,
         type: 'javascript/auto',
         resolve: {
