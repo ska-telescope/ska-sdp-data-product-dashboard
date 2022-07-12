e91d83e4764a98746af683cf6a214acb7fa9b41c
Andre Odendaal <andre@vivosa.co.za>
2022-07-13 00:52:26 +0200

    NAL-169: Updated linting

--- src/components/Ska_sdp_data_product_dashboard/Ska_sdp_data_product_dashboard.test.jsx
+++ src/components/Ska_sdp_data_product_dashboard/Ska_sdp_data_product_dashboard.test.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Ska_sdp_data_product_dashboard from './Ska_sdp_data_product_dashboard';
+import DataProductDashboard from './Ska_sdp_data_product_dashboard';
 
 describe('Login Component', () => {
   it('renders without crashing', () => {
-    shallow(<Ska_sdp_data_product_dashboard />);
+    shallow(<DataProductDashboard />);
   });
 });
