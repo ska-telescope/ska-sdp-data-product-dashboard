944b25f4e9611c79a15af9ae63318824e9ef6487
Andre Odendaal <andre@vivosa.co.za>
2022-07-13 12:16:37 +0200

    NAL-169: Linting updates

--- src/components/Ska_sdp_data_product_dashboard/Ska_sdp_data_product_dashboard.jsx
+++ src/components/Ska_sdp_data_product_dashboard/Ska_sdp_data_product_dashboard.jsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
@@ -10,10 +9,6 @@ import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import FolderIcon from '@mui/icons-material/Folder';
 import DownloadIcon from '@mui/icons-material/Download';
-
-const Demo = styled('div')(({ theme }) => ({
-  backgroundColor: theme.palette.background.paper
-}));
 
 const DataProductDashboard = () => {
   const [filelist, setFilelist] = React.useState([]);
@@ -43,26 +38,25 @@ const DataProductDashboard = () => {
           <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
             Available data products:
           </Typography>
-          <Demo>
-            <List dense>
-              {filelist.map(file => {
-                return (
-                  <ListItem
-                    secondaryAction={
-                      <IconButton edge="end" aria-label="download">
-                        <DownloadIcon />
-                      </IconButton>
-                    }
-                  >
-                    <ListItemIcon>
-                      <FolderIcon />
-                    </ListItemIcon>
-                    <ListItemText primary={file} />
-                  </ListItem>
-                );
-              })}
-            </List>
-          </Demo>
+          <List dense>
+            {filelist.map((file, key) => {
+              return (
+                <ListItem
+                  key={key}
+                  secondaryAction={
+                    <IconButton edge="end" aria-label="download">
+                      <DownloadIcon />
+                    </IconButton>
+                  }
+                >
+                  <ListItemIcon>
+                    <FolderIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={file} />
+                </ListItem>
+              );
+            })}
+          </List>
         </Grid>
       </Grid>
     </Box>
