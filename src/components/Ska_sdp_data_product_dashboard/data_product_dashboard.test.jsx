bfef052b05d7a84df540eb29f7146e2bdf112ce9
Andre Odendaal <andre@vivosa.co.za>
2022-07-29 09:38:36 +0200

    NAL-206: Added test on API & file list

---
+++ src/components/Ska_sdp_data_product_dashboard/data_product_dashboard.test.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import DataProductDashboard from './data_product_dashboard';
+
+describe('Login Component', () => {
+  it('renders without crashing', () => {
+    shallow(<DataProductDashboard />);
+  });
+});
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it('renders user data', async () => {
+  const fakeFileList = { filelist: [{ id: 0, filename: 'testfile.txt' }] };
+  jest.spyOn(global, 'fetch').mockImplementation(() =>
+    Promise.resolve({
+      json: () => Promise.resolve(fakeFileList)
+    })
+  );
+
+  // Use the asynchronous version of act to apply resolved promises
+  await act(async () => {
+    render(<DataProductDashboard />, container);
+  });
+
+  expect(container.textContent).toContain('testfile.txt');
+
+  // remove the mock to ensure tests are completely isolated
+  global.fetch.mockRestore();
+});
