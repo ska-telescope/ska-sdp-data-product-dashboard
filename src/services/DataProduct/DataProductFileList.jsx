ccc952d541cdd9f7d9ced264afee1ca7515e0b4e
trevor.swain <trevor.swain@cgi.com>
2022-08-23 12:25:10 +0100

    NAL-206: Removed unnecessary variable

--- src/services/DataProduct/DataProductFileList.jsx
+++ src/services/DataProduct/DataProductFileList.jsx
@@ -6,13 +6,12 @@ async function DataProductFileList() {
     const apiUrl = process.env.SKA_SDP_DATA_PRODUCT_API_URL;
     const apiPort = process.env.SKA_SDP_DATA_PRODUCT_API_PORT;
     try {
-      const data = await axios.get(`${apiUrl}:${apiPort}/filelist`, {
+      return await axios.get(`${apiUrl}:${apiPort}/filelist`, {
         headers: {
           Accept: 'application/json',
           'Content-Type': 'application/json'
         }
       });
-      return data.data;
     } catch (e) {
       // eslint-disable-next-line no-console
       // console.error('Catch error', e);
