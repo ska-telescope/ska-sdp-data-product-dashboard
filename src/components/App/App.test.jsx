e6dcac853689314935bdeeb37163d5b4a1aa3b6c
Karini Govender <karini@vivosa.co.za>
2022-11-14 12:43:22 +0200

    NAL-236: Fixed failing test

--- src/components/App/App.test.jsx
+++ src/components/App/App.test.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import App from './App';
 
 describe('App Component', () => {
+  beforeEach(() => {
+    process.env.REACT_APP_SKA_SDP_DATA_PRODUCT_DUMMY_DATA = true;
+  });
+
+  afterEach(() => {
+    process.env.REACT_APP_SKA_SDP_DATA_PRODUCT_DUMMY_DATA = false;
+    cleanup();
+  });
+  
   it('renders without crashing', () => {
     render(<App />);
   });
