45771a2e86f94c302cf2860ae19fc71bc79fa707
Gallacher <chloe.gallacher@cgi.com>
2023-01-27 14:57:47 +0000

    NAL-392: Replacing hard-coded string with variable TEXT_NO_API

--- src/components/DataProductDashboard/DataProductDashboard.cy.jsx
+++ src/components/DataProductDashboard/DataProductDashboard.cy.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import DataProductDashboard from './DataProductDashboard'
 
+const TEXT_NO_API = "SDP Data API not available"
 describe('<DataProductDashboard />', () => {
   it('Data Product Dashboard renders correctly when data is unavailable', () => {
     // see: https://on.cypress.io/mounting-react
     cy.mount(<DataProductDashboard />)
     cy.findAllByTestId("WarningIcon").should("be.visible")
-    cy.findByText("SDP Data API not available").should("be.visible")
+    cy.findByText(TEXT_NO_API).should("be.visible")
   })
 })
