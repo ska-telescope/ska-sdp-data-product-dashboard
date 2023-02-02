019dde1f82cd1b95c7d334fd6e2c4a0e4102a5c5
Gallacher <chloe.gallacher@cgi.com>
2023-02-02 09:57:24 +0000

    NAL-370: Added new dp dashboard test scenario for downloading data

--- src/components/DataProductDashboard/DataProductDashboard.cy.jsx
+++ src/components/DataProductDashboard/DataProductDashboard.cy.jsx
@@ -7,6 +7,9 @@ const TEXT_NO_API = "SDP Data API not available";
 const TITLE = "Mocked Data Products";
 const PROD_1 = "pb_id_1";
 const PROD_2 = "pb_id_2";
+const TEST_DATA_FILE_1 = "TestDataFile1.txt";
+const DOWNLOAD_BUTTON = "DownloadIcon";
+
 
 describe('<DataProductDashboard />', () => {
 
@@ -24,5 +27,20 @@ describe('<DataProductDashboard />', () => {
     cy.findByText(TITLE).should("be.visible") 
     cy.findByText(PROD_1).should("be.visible") 
     cy.findByText(PROD_2).should("be.visible")
+  })
+
+
+  it('Data is available for download on Data Product Dashboard', () => {
+    cy.stub(axios, 'get').returns(MockData).as('fetch')
+    cy.mount(<DataProductDashboard />)
+    cy.get('@fetch').should('have.been.called')
+
+    cy.findByText(PROD_1).should("be.visible")
+    cy.findByText(PROD_1).click()
+    cy.findByText(TEST_DATA_FILE_1).should("be.visible")
+
+    cy.findByText(TEST_DATA_FILE_1).click()
+    cy.findByTestId(DOWNLOAD_BUTTON).click()
+    cy.readFile('cypress/downloads/' + TEST_DATA_FILE_1).should('contain', 'Error')
   })
 })
