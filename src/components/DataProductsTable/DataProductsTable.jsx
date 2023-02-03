c60313ea726c810d788f6d0c1a7bd13becbde999
james-strauss-uwa <james.strauss@uwa.edu.au>
2023-02-03 16:42:10 +0800

    YAN-1231: Make FetchDataProductList return dummy data. Use dummy data to populate DataGrid. Handle rowClick events on DataGrid

--- src/components/DataProductsTable/DataProductsTable.jsx
+++ src/components/DataProductsTable/DataProductsTable.jsx
@@ -1,4 +1,7 @@
 import * as React from 'react';
+
+import { Typography } from '@mui/material';
+import WarningIcon from '@mui/icons-material/Warning';
 import { DataGrid } from "@mui/x-data-grid";
 
 const columns = [
@@ -7,68 +10,34 @@ const columns = [
   { field: "interface", headerName: "interface", width: 400 }
 ];
 
-const rows = [
-  {
-    id: 1,
-    interface: "http://schema.skao.int/ska-data-product-meta/0.1",
-    execution_block: "eb-m001-20191031-12345"
-  },
-  {
-    id: 2,
-    interface: "http://schema.skao.int/ska-data-product-meta/0.1",
-    execution_block: "eb-m001-20191031-12345"
-  },
-  {
-    id: 3,
-    interface: "http://schema.skao.int/ska-data-product-meta/0.1",
-    execution_block: "eb-m001-20191031-12345"
-  },
-  {
-    id: 4,
-    interface: "http://schema.skao.int/ska-data-product-meta/0.1",
-    execution_block: "eb-m001-20191031-12345"
-  },
-  {
-    id: 5,
-    interface: "http://schema.skao.int/ska-data-product-meta/0.1",
-    execution_block: "eb-m001-20191031-12345"
-  },
-  {
-    id: 6,
-    interface: "http://schema.skao.int/ska-data-product-meta/0.1",
-    execution_block: "eb-m001-20191031-12345"
-  },
-  {
-    id: 7,
-    interface: "http://schema.skao.int/ska-data-product-meta/0.1",
-    execution_block: "eb-m001-20191031-12345"
-  },
-  {
-    id: 8,
-    interface: "http://schema.skao.int/ska-data-product-meta/0.1",
-    execution_block: "eb-m001-20191031-12345"
-  },
-  {
-    id: 9,
-    interface: "http://schema.skao.int/ska-data-product-meta/0.1",
-    execution_block: "eb-m001-20191031-12345"
-  },
-  {
-    id: 10,
-    interface: "http://schema.skao.int/ska-data-product-meta/0.1",
-    execution_block: "eb-m001-20191031-12345"
-  }
-];
+const DataProductsTable = (jsonDataProducts, handleSelectedNode) => {
+  console.log("DataProductsTable", jsonDataProducts);
 
-const DataProductsTable = () => {
+  // if jsonDataProducts contains no data, display a warning instead
+  if (jsonDataProducts === null){
+    return (
+      <>
+        <Typography sx={{ fontSize: 25, display: "flex", justifyContent: "center" }} color="error" gutterBottom>
+          <WarningIcon sx={{ fontSize: "35px" }}  />
+          {" "}
+          {"SDP Data API not available"}
+        </Typography>
+      </>
+    );
+  }
+
+  // TODO: dynamically build the columns array based on the contents of jsonDataProducts?
+
+
   return (
     <div style={{ height: 400, width: "100%" }}>
       <DataGrid
-        rows={rows}
+        rows={jsonDataProducts}
         columns={columns}
         pageSize={5}
         rowsPerPageOptions={[5]}
         checkboxSelection
+        onRowClick={handleSelectedNode}
       />
     </div>
   );
--- src/services/FetchDataProductList/FetchDataProductList.jsx
+++ src/services/FetchDataProductList/FetchDataProductList.jsx
@@ -5,12 +5,29 @@ const FetchDataProductList = async () => {
   const URL_LIST = '/dataproductlist';
 
   try {
-    return await axios.get(`${apiUrl}${URL_LIST}`, {
+    const results = await axios.get(`${apiUrl}${URL_LIST}`, {
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json'
       }
     });
+
+    /* dummy data from the API */
+    const dummyData = [
+      {"id": 1, "interface": "http://schema.skao.int/ska-data-product-meta/0.1", "execution_block": "eb-m001-20191031-12345"},
+      {"id": 2, "interface": "http://schema.skao.int/ska-data-product-meta/0.1", "execution_block": "eb-m001-20191031-12345"},
+      {"id": 3, "interface": "http://schema.skao.int/ska-data-product-meta/0.1", "execution_block": "eb-m001-20191031-12345"},
+      {"id": 4, "interface": "http://schema.skao.int/ska-data-product-meta/0.1", "execution_block": "eb-m001-20191031-12345"},
+      {"id": 5, "interface": "http://schema.skao.int/ska-data-product-meta/0.1", "execution_block": "eb-m001-20191031-12345"},
+      {"id": 6, "interface": "http://schema.skao.int/ska-data-product-meta/0.1", "execution_block": "eb-m001-20191031-12345"},
+      {"id": 7, "interface": "http://schema.skao.int/ska-data-product-meta/0.1", "execution_block": "eb-m001-20191031-12345"},
+      {"id": 8, "interface": "http://schema.skao.int/ska-data-product-meta/0.1", "execution_block": "eb-m001-20191031-12345"},
+      {"id": 9, "interface": "http://schema.skao.int/ska-data-product-meta/0.1", "execution_block": "eb-m001-20191031-12345"},
+      {"id": 10, "interface": "http://schema.skao.int/ska-data-product-meta/0.1", "execution_block": "eb-m001-20191031-12345"}
+    ];
+
+    /* here we temporarily return the dummyData in place of the actual data received from the API */
+    return dummyData;
   } catch (e) {
     return "API unreachable, SDP data not available";
   }
