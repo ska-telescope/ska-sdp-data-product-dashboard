b335b8b079be9a1c792e16bb1af6359ed320ff86
Andre Odendaal <andre@vivosa.co.za>
2023-02-06 16:56:38 +0200

    NAL-385: Update of search button functions

--- src/components/SearchCard/SearchCard.jsx
+++ src/components/SearchCard/SearchCard.jsx
@@ -1,14 +1,16 @@
 import * as React from 'react';
 import { Box, Button, Card, CardActions, CardContent, Typography, TextField } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
-import DownloadDataProduct from '../../services/DownloadDataProduct/DownloadDataProduct';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import FetchDataProductList from "../../services/FetchDataProductList/FetchDataProductList"
 
 const DownloadCard = () => {
-  const [startDate, setStartDate] = React.useState("2015-01-01");
-  const [endDate, setEndDate] = React.useState("2050-12-12");
+  const [startDate, updateStartDate] = React.useState("2015-01-01");
+  const [endDate, updateEndDate] = React.useState("2050-12-12");
+  const [metadataKey, updateMetadataKey] = React.useState("execution_block");
+  const [metadataValue, updateMetadataValue] = React.useState("eb-m001-20191031-12345");
 
   return (
     <Box m={1}>
@@ -22,7 +24,7 @@ const DownloadCard = () => {
               label="Start date"
               value={startDate}
               onChange={(newValue) => {
-                setStartDate(newValue);
+                updateStartDate(newValue);
               }}
               renderInput={(params) => <TextField {...params} />}
             />
@@ -30,7 +32,7 @@ const DownloadCard = () => {
               label="End Date"
               value={endDate}
               onChange={(newValue) => {
-                setEndDate(newValue);
+                updateEndDate(newValue);
               }}
               renderInput={(params) => <TextField {...params} />}
             />
@@ -38,19 +40,25 @@ const DownloadCard = () => {
               required
               id="outlined-required"
               label="Key"
-              defaultValue="execution_block"
+              defaultValue={metadataKey}
+              onChange={(newValue) => {
+                updateMetadataKey(newValue);
+              }}
             />
             <TextField
               required
               id="outlined-required"
               label="Value"
-              defaultValue="eb-m001-20191031-12345"
+              defaultValue={metadataValue}
+              onChange={(newValue) => {
+                updateMetadataValue(newValue);
+              }}
             />      
           </LocalizationProvider>
   
         </CardContent>
         <CardActions>
-          <Button variant="outlined" color="secondary" onClick={() => DownloadDataProduct()}>
+          <Button variant="outlined" color="secondary" onClick={() => FetchDataProductList(startDate, endDate, metadataKey, metadataValue)}>
             <SearchIcon />
             Search
           </Button>
