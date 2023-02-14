31cf4375b7a2dc97b0502496323c2a477f96e373
t.swain <trevor.swain@cgi.com>
2023-02-14 15:14:23 +0000

    NAL-413 : Code coverage available via the developer tools

--- cypress.config.js
+++ cypress.config.js
@@ -8,16 +8,13 @@ module.exports = defineConfig({
     },
   },
 
-  component: {
-    devServer: {
-      framework: "create-react-app",
-      bundler: "webpack",
-    },
-  },
-
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('@cypress/code-coverage/task')(on, config)
+      return config
     },
   },
+  env: {
+    codeCoverageTasksRegistered: true,
+  }
 });
