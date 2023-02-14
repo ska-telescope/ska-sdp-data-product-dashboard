4b54030905291eb0f3abd3ad16c7ed8d4e3f807f
gallacherc <chloe.gallacher@cgi.com>
2023-02-14 12:04:32 +0000

    NAL-397: Updating dp dashboard E2E tests to include both available products

--- cypress/e2e/DataProductDashboard.cy.js
+++ cypress/e2e/DataProductDashboard.cy.js
@@ -5,8 +5,14 @@ context('Select and download data product', () => {
     cy.visit(Constants.LOCAL_HOST)
   })
 
-  it('Select data product and download file', () => {
+  it('Select data product 1 and download file', () => {
     cy.findByText("1").click()
+    cy.findByTestId(Constants.DOWNLOAD_ICON).click()
+    cy.readFile('cypress/downloads/' + Constants.TEST_DATA_FILE_1).should('contain', 'Error')
+  })
+
+  it('Select data product 2 and download file', () => {
+    cy.findByText("2").click()
     cy.findByTestId(Constants.DOWNLOAD_ICON).click()
     cy.readFile('cypress/downloads/' + Constants.TEST_DATA_FILE_1).should('contain', 'Error')
   })
