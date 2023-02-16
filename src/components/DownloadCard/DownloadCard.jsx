ec74769a2083a5729d2a47715802da0ba36b8266
gallacherc <chloe.gallacher@cgi.com>
2023-02-16 20:24:07 +0000

    NAL-274: Modifying donwload card to use translation file rather than hard coded text

--- src/components/DownloadCard/DownloadCard.jsx
+++ src/components/DownloadCard/DownloadCard.jsx
@@ -2,15 +2,17 @@ import * as React from 'react';
 import { Box, Button, Card, CardActions, CardContent, Typography } from '@mui/material';
 import DownloadIcon from '@mui/icons-material/Download';
 import DownloadDataProduct from '../../services/DownloadDataProduct/DownloadDataProduct';
+import { useTranslation } from 'react-i18next';
 
 const DownloadCard = (selectedFileNames) => {
   const { fileName } = selectedFileNames
+  const { t } = useTranslation();
   return (
     <Box m={1}>
       <Card variant="outlined" sx={{ minWidth: 275 }}>
         <CardContent>
           <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            Selected file:
+            {t('selectFile')}
           </Typography>
           <Typography variant="h5" component="div">
             {fileName} 
