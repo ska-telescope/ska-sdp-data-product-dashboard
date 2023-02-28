922b9772c5be8bc6826050b0bf9688f95012b4ea
gallacherc <chloe.gallacher@cgi.com>
2023-02-28 12:50:27 +0000

    NAL-422: Updated configurations to enable cypress code coverage

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,7 +2,7 @@ module.exports = Object.freeze({
   TEXT_NO_API: "error.API_NO_DATA",
   METADATA_TITLE: "label.metaData",
   PROD_1: "eb-m001-20191031-12345",
-  PROD_2: "eb-m001-20221212-12345",
+  PROD_2: "eb-m002-20221212-12345",
   TEST_DATA_FILE_1: "TestDataFile1.txt",
   DOWNLOAD_ICON: "DownloadIcon",
   SELECTED_FILE_TITLE: "prompt.selectFile",
