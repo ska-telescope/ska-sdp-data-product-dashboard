f19b322c62ceb4cb3ad5b615cb61385993791b1d
james-strauss-uwa <james.strauss@uwa.edu.au>
2023-04-19 15:55:54 +0800

    YAN-1263: Remove hard-coded section titles in MetaDataComponent. Instead programmatically generate all sections that exist in metadata

--- src/components/MetaDataComponent/MetaDataComponent.jsx
+++ src/components/MetaDataComponent/MetaDataComponent.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Box, Card, CardContent, Divider, Typography } from '@mui/material';
 
-export const SECTIONS= [ 'interface', 'execution_block', 'context', 'config', 'files'];
-
 function sectionDisplay(title, data) {  
   const isString = typeof data === 'string';
   return (
@@ -18,6 +16,12 @@ function sectionDisplay(title, data) {
   );
 }
 
+function generateSections(metaData) {
+  return Object.keys(metaData.metaData).map((sectionTitle) =>
+    metaData?.metaData[sectionTitle] && sectionDisplay(sectionTitle, metaData?.metaData[sectionTitle])
+  );
+}
+
 const MetaDataComponent = (metaData) => {  
   const { t } = useTranslation();
   
@@ -26,11 +30,7 @@ const MetaDataComponent = (metaData) => {
       <Card variant="outlined" sx={{ minWidth: 275 }}>
         <CardContent>
           <Typography variant="h4" component="div">{t('label.metaData')}</Typography>
-          {metaData?.metaData?.interface && sectionDisplay(SECTIONS[0], metaData?.metaData?.interface)}
-          {metaData?.metaData?.execution_block && sectionDisplay(SECTIONS[1], metaData?.metaData?.execution_block)}
-          {metaData?.metaData?.context && sectionDisplay(SECTIONS[2], metaData?.metaData?.context)}
-          {metaData?.metaData?.config && sectionDisplay(SECTIONS[3], metaData?.metaData?.config)}
-          {metaData?.metaData?.files && sectionDisplay(SECTIONS[4], metaData?.metaData?.files)}
+          {generateSections(metaData)}
         </CardContent>
       </Card>
     </Box>
