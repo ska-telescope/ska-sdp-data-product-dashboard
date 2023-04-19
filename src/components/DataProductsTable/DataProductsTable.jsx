322f8dc1514832e3322fcc27c193823e85499c45
james-strauss-uwa <james.strauss@uwa.edu.au>
2023-04-19 14:53:02 +0800

    YAN-1263: Add columns to DataProductsTable for additional attributes found in the data returned from a dataproductsearch

--- src/components/DataProductsTable/DataProductsTable.jsx
+++ src/components/DataProductsTable/DataProductsTable.jsx
@@ -11,8 +11,10 @@ const DataProductsTable = (jsonDataProducts, handleSelectedNode) => {
     { field: "id", headerName: t("column.id"), width: 50 },
     { field: "execution_block", headerName: t("column.ex_block"), width: 200 },
     { field: "date_created", headerName: t("column.dateCreated"), width: 100 },
-    { field: "interface", headerName: t("column.id"), width: 400 }  
+    { field: "interface", headerName: t("column.interface"), width: 400 }
   ];
+
+  const ignore_columns_names = ["dataproduct_file", "metadata_file"];
 
   // if jsonDataProducts contains an warning string, display the warning instead
   if (typeof jsonDataProducts === "undefined" || typeof jsonDataProducts === "string"){
@@ -29,11 +31,38 @@ const DataProductsTable = (jsonDataProducts, handleSelectedNode) => {
     );
   }
 
+  // create a 'deep' copy of the columns array, to which we can add additional columns
+  // for data found in jsonDataProducts
+  const extendedColumns = structuredClone(columns);
+
+  // if jsonDataProducts contains additional attributes, assume those attributes were part
+  // of the user's query, and display them
+  if (jsonDataProducts.length > 0){
+    for (const key of Object.keys(jsonDataProducts[0])){
+      // skip keys in ignore_column_names
+      if (ignore_columns_names.includes(key)){
+        continue;
+      }
+
+      // skip keys already in columns
+      if (columns.map(a => a.field).includes(key)){
+        continue;
+      }
+
+      // add new column to extendedColumns
+      extendedColumns.push({
+        field: key,
+        headerName: key,
+        width: 200
+      });
+    }
+  }
+
   return (
     <Box m={1} sx={{ height: '43vh', width: "100%" }}>
       <DataGrid
         rows={jsonDataProducts}
-        columns={columns}
+        columns={extendedColumns}
         pageSize={5}
         rowsPerPageOptions={[5]}
         checkboxSelection={false}
