38048fadb0900491efaf9dd49bbcc7ce0fe8dfbc
t.swain <trevor.swain@cgi.com>
2023-07-05 11:35:12 +0100

    NAL-552 : Updated i18nto use namespace

--- src/components/DataProductsTable/DataProductsTable.jsx
+++ src/components/DataProductsTable/DataProductsTable.jsx
@@ -3,10 +3,10 @@ import { useTranslation } from 'react-i18next';
 import { Box, Grid } from '@mui/material';
 import { InfoCard } from '@ska-telescope/ska-gui-components';
 import { DataGrid } from "@mui/x-data-grid";
-import Constants from '../../constants/constants';
+import Constants from '../../utils/constants';
 
-const DataProductsTable = (jsonDataProducts, handleSelectedNode) => {
-  const { t } = useTranslation();
+const DataProductsTable = (jsonDataProducts, updating, handleSelectedNode) => {
+  const { t } = useTranslation('dpd');
 
   const columns = [
     { field: "execution_block", headerName: t("column.execution_block"), width: 200 },
@@ -15,15 +15,8 @@ const DataProductsTable = (jsonDataProducts, handleSelectedNode) => {
 
   const ignore_columns_names = ["dataproduct_file", "metadata_file"];
 
-  // if jsonDataProducts contains an warning string, display the warning instead
-  if (typeof jsonDataProducts === "undefined" || typeof jsonDataProducts === "string"){
-    return (
-      <Box m={1} sx={{ height: '43vh', width: "100%" }}>
-        <Grid container alignItems="center" justifyContent="center">
-          <InfoCard fontSize={25} level={1} message={t("error.API_NO_DATA")} />
-        </Grid>
-      </Box>
-    );
+  const haveData = () => {
+    return (typeof jsonDataProducts === "object" && jsonDataProducts.length > 0 );
   }
 
   // create a 'deep' copy of the columns array, to which we can add additional columns
@@ -32,7 +25,7 @@ const DataProductsTable = (jsonDataProducts, handleSelectedNode) => {
 
   // if jsonDataProducts contains additional attributes, assume those attributes were part
   // of the user's query, and display them
-  if (jsonDataProducts.length > 0){
+  if (haveData() && jsonDataProducts.length > 0){
     for (const dataproduct in jsonDataProducts){
       for (const key of Object.keys(jsonDataProducts[dataproduct])){
         // skip keys in ignore_column_names
@@ -55,18 +48,38 @@ const DataProductsTable = (jsonDataProducts, handleSelectedNode) => {
     }
   }
 
+  function RenderInfo(value, msg) {
+    return (
+      <Box m={1} sx={{ height: '43vh', width: "100%" }}>
+        <Grid container alignItems="center" justifyContent="center">
+          <InfoCard fontSize={25} level={value} message={t(msg)} />
+        </Grid>
+      </Box>
+    );
+  }
+
+  function RenderData() {
+    return (
+      <Box m={1} sx={{ height: `calc(100vh - ${Constants.HEADER_HEIGHT + Constants.FOOTER_HEIGHT + 20 + Constants.DATA_STORE_BOX_HEIGHT + 20 }px)`, width: "100%" }}>
+        <DataGrid 
+          aria-label="Data Product Grid"
+          rows={jsonDataProducts}
+          columns={extendedColumns}
+          pageSize={10}
+          rowsPerPageOptions={[10]}
+          checkboxSelection={false}
+          onRowClick={handleSelectedNode}
+        />
+      </Box>
+    );
+  }
+
   return (
-    <Box m={1} sx={{ height: `calc(100vh - ${Constants.HEADER_HEIGHT + Constants.FOOTER_HEIGHT + 20 + Constants.DATA_STORE_BOX_HEIGHT + 20 }px)`, width: "100%" }}>
-      <DataGrid 
-        aria-label="Data Product Grid"
-        rows={jsonDataProducts}
-        columns={extendedColumns}
-        pageSize={10}
-        rowsPerPageOptions={[10]}
-        checkboxSelection={false}
-        onRowClick={handleSelectedNode}
-      />
-    </Box>
+    <>
+      {updating && RenderInfo(2, "info.fetching")}
+      {!updating && !haveData() && RenderInfo(1, "error.API_NO_DATA")}
+      {!updating && haveData() && RenderData()}
+    </>
   );
 }
 
