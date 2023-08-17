37bcf33e67ccf70ee98c8954cc6f96b0c392c9c3
t.swain <trevor.swain@cgi.com>
2023-08-17 16:21:16 +0100

    NAL-711 : Adjusted version number

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -6,9 +6,9 @@ copyright = '2023, SKAO'
 author = 'SKAO, (Andre Odendaal)'
 
 # The short X.Y version
-version = '0.4.5'
+version = '0.4.4'
 # The full version, including alpha/beta/rc tags
-release = '0.4.5'
+release = '0.4.4'
 
 # -- General configuration ---------------------------------------------------
 
