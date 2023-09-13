d4dffdd5dba8cbe1651f58862e57b3fabe35f236
Andre Odendaal <andre@vivosa.co.za>
2023-09-13 10:47:06 +0200

    NAL-729: Updated download endpoint to stream data

--- src/components/DownloadCard/DownloadCard.jsx
+++ src/components/DownloadCard/DownloadCard.jsx
@@ -1,13 +1,54 @@
-import * as React from 'react';
+import React from "react";
 import { Box, Card, CardContent, Grid, Typography } from '@mui/material';
 import DownloadIcon from '@mui/icons-material/Download';
 import { Button, DataTree } from '@ska-telescope/ska-gui-components';
-import DownloadDataProduct from '../../services/DownloadDataProduct/DownloadDataProduct';
+// import DownloadDataProduct from '../../services/DownloadDataProduct/DownloadDataProduct';
 import { useTranslation } from 'react-i18next';
+import streamSaver from "streamsaver";
 
-const DownloadCard = (selectedFileNames, metaData) => {
+
+function DownloadCard(selectedFileNames, metaData) {
+  // use state to track the download progress
+  const URL_DOWNLOAD = '/download';
+  const apiUrl = process.env.REACT_APP_SKA_SDP_DATAPRODUCT_API_URL;
   const { t } = useTranslation('dpd');
-  
+  const options = {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept-Encoding': 'gzip',
+    },
+    body: JSON.stringify(selectedFileNames)
+  };
+
+  // handle the click event of the button
+  const handleClick = async () => {
+    try {
+      // make a request to the endpoint with the file object
+      const response = await fetch(`${apiUrl}${URL_DOWNLOAD}`,options);
+      // create a write stream using streamSaver library
+      const fileStream = streamSaver.createWriteStream(selectedFileNames.relativePathName + ".tar");
+      const readableStream = response.body;
+
+      // pipe the stream
+      if (window.WritableStream && readableStream.pipeTo) {
+        await readableStream.pipeTo(fileStream);
+      } else {
+        // fallback
+        // declare writer here
+        const writer = fileStream.getWriter();
+        const reader = response.body.getReader();
+        const pump = () =>
+          reader.read().then((res) =>
+            res.done ? writer.close() : writer.write(res.value).then(pump)
+          );
+        pump();
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
     {selectedFileNames?.relativePathName !== '' && 
@@ -25,7 +66,7 @@ const DownloadCard = (selectedFileNames, metaData) => {
                   color="secondary"
                   icon={<DownloadIcon />}
                   label={t('button.download')}
-                  onClick={() => DownloadDataProduct(selectedFileNames)}
+                  onClick={handleClick}
                   testId="downloadButton"
                   toolTip=""
                   variant="outlined"
