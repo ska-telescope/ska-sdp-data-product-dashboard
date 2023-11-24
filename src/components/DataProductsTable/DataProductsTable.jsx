a0900672b27cf33f6733d030608cb7350efbf7cb
Karini Govender <karini@vivosa.co.za>
2023-11-24 05:09:05 +0200

    NAL-850: Got it working

--- src/components/DataProductsTable/DataProductsTable.jsx
+++ src/components/DataProductsTable/DataProductsTable.jsx
@@ -1,4 +1,5 @@
-import {React, useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios  from 'axios';
 import { useTranslation } from 'react-i18next';
 import { Box, Grid } from '@mui/material';
 import { DataGrid, InfoCard } from '@ska-telescope/ska-gui-components';
@@ -6,40 +7,37 @@ import { tableHeight, SKA_SDP_DATAPRODUCT_API_URL } from "../../utils/constants"
 
 const DataProductsTable = (jsonDataProducts, updating, apiRunning, handleSelectedNode) => {
   const { t } = useTranslation('dpd');
-  const [columnInfo, setData] = useState(null);
+  const [columnInfo, setColumnInfo] = useState([]); 
 
-  // const columns = [
-  //   { field: "execution_block", headerName: t("execution_block", { ns: 'ivoa' }), width: 200 },
-  //   { field: "date_created", headerName: t("date_created", { ns: 'ivoa' }), width: 100 }
-  // ];
+  const fetchData = async () => {
+    const { data } = await axios.get(`${SKA_SDP_DATAPRODUCT_API_URL}/layout`);
+    setColumnInfo(data);
+  };
 
-  const ignore_columns_names = ["dataproduct_file", "metadata_file"];
-
-  fetch(`${SKA_SDP_DATAPRODUCT_API_URL}/layout`)
-    .then(response => response.json())
-    .then(json => setData(json))
-    .catch(error => console.error(error));
-
-  console.log(columnInfo);
+  useEffect( () => { 
+    fetchData();
+  }, []);
 
   const haveData = () => {
     return (typeof jsonDataProducts === "object" && jsonDataProducts.length > 0 );
   }
   // Create Header name from column_name
   const headerText = (key) => {
-    const tmp = key.split('.');
-    return t(tmp[tmp.length - 1], { ns: 'ivoa' });
+    const tmp = key?.split('.');
+    return t(tmp[tmp?.length - 1], { ns: 'ivoa' });
   }
   // Create the array of column names and html from /layout call
   const extendedColumns = [];
-  for (const column in columnInfo){
-      extendedColumns.push({
-        field: column["name"],
-        headerName: headerText(column["name"]),
-        width: column["width"]
-      })
+  if(columnInfo !== undefined){
+    for (const column of columnInfo){
+        extendedColumns.push({
+          field: column["name"],
+          headerName: headerText(column["name"]),
+          width: column["width"]
+        })
+    }
   }
-  
+
   function RenderInfo(value, msg) {
     return (
       <Box m={1} sx={{ height: '43vh', width: "100%" }}>
