88ecf5e93f34e7bd09df6cab99db6b1107babea5
Andre Odendaal <andre@vivosa.co.za>
2024-02-23 10:27:53 +0200

    REL-1251: Created release v0.7.0

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -6,9 +6,9 @@ copyright = '2024, SKAO'
 author = 'SKAO'
 
 # The short X.Y version
-version = '0.6.2'
+version = '0.7.0'
 # The full version, including alpha/beta/rc tags
-release = '0.6.2'
+release = '0.7.0'
 
 # -- General configuration ---------------------------------------------------
 
