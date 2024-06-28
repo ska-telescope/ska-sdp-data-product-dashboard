027ff69f1e05abe917d1fefa771a4bd58cfe12bf
Andre Odendaal <andre@vivosa.co.za>
2024-06-28 15:12:26 +0200

    NAL-1119: Updated nginx_env_config script to not require .env file, and removed .env from repository

--- nginx_env_config.sh
+++ nginx_env_config.sh
@@ -1,31 +1,97 @@
 #!/bin/env bash
 
-source /vault/secrets/config
-if [ ! -f /.env ]; then
-    echo "No Setup Required, mising /.env"
-    exit
+################################################################################
+# Setup
+################################################################################
+
+if [ -z "$ENV_SEPARATOR" ]; then
+    ENV_SEPARATOR=':'
+fi
+if [ -z "$ENV_TYPE_FILE" ]; then
+    ENV_TYPE_FILE='/expected_env_names'
+fi
+if [ -z "$ENV_JS_OUTPUT_LOCATION" ]; then
+    ENV_JS_OUTPUT_LOCATION='/usr/share/nginx/html/env.js'
 fi
 
+################################################################################
+# End Setup
+################################################################################
+
+read_vault()
 {
-    echo 'window.env = {'
-    count=0
-    while read -r line; do
-        var="$(echo "$line" | cut -d '=' -f1)"
-        default="$(echo "$line" | cut -d '=' -f2)"
-        type="$(echo "$line" | cut -d '=' -f2  | cut -c1)"
-        if [[ "$type" == "'" ]]; then
-            default="$(echo "$default" | cut -d"'" -f 2)"
-        fi
-        val="${!var:-$default}"
-        if [[ count -gt 0 ]]; then
-            echo ','
-        fi
-        if [[ "$type" == "'" ]]; then
-            echo -n "  $var: '$val'"
-        else
-            echo -n "  $var: $val"
-        fi
-        count=$((count+1))
-    done < /.env
-    echo -e "\n}"
-} > /usr/share/nginx/html/env.js
+    if [ -f /vault/secrets/config ]; then
+        echo "Using vault config"
+        source /vault/secrets/config
+    else
+        echo "No vault secrets found, skipping loading"
+    fi
+}
+
+get_type_default()
+{
+    if [[ "$1" == "str" ]]; then
+        echo ''
+    elif [[ "$1" == "int" ]]; then
+        echo '0'
+    elif [[ "$1" == "bool" ]]; then
+        echo 'false' # quote false to avoid the builtin running
+    else
+        echo ''
+    fi
+}
+
+output_js_line()
+{
+    # add explicit quoting if we are a string,
+    if [[ "$1" == "str" ]]; then
+        echo -n "  $2: '$3'"
+    else
+        echo -n "  $2: $3"
+    fi
+}
+
+create_env_js()
+{
+    {
+        echo 'window.env = {'
+        count=0
+
+        # read each line of the input file
+        while read -r line; do
+
+            # Get expected name, and type
+            var="$(echo "$line" | cut -d "$ENV_SEPARATOR" -f1)"
+            type="$(echo "$line" | cut -d "$ENV_SEPARATOR" -f2)"
+
+            # Get default values
+            default="$(get_type_default "$type")"
+
+            # Check if the variable name stored in `var` exists and isn't blank
+            # if it exists and isn't blank use it
+            # otherwise use the value in the `default` variable
+            val="${!var:-$default}"
+
+            # if we are after the first line, add a comma
+            if [[ count -gt 0 ]]; then
+                echo ','
+            fi
+
+            output_js_line "$type" "$var" "$val"
+
+            count=$((count+1))
+        done < "$ENV_TYPE_FILE"
+
+        echo -e "\n}"
+    } > "$ENV_JS_OUTPUT_LOCATION"
+}
+
+read_vault
+
+# Check if we have a ENV type list
+if [ -f "$ENV_TYPE_FILE" ]; then
+    create_env_js
+    echo "ENV added to $ENV_JS_OUTPUT_LOCATION"
+else
+    echo "No Setup Required, mising '$ENV_TYPE_FILE'"
+fi
