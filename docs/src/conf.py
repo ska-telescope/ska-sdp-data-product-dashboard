bf77932b55ea855941a143ab376ccdfbf762a75b
Andre Odendaal <andre@vivosa.co.za>
2024-09-26 11:11:03 +0200

    REL-1764: Created release v0.10.0

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -6,9 +6,9 @@ copyright = '2024, SKAO'
 author = 'SKAO'
 
 # The short X.Y version
-version = '0.9.0'
+version = '0.10.0'
 # The full version, including alpha/beta/rc tags
-release = '0.9.0'
+release = '0.10.0'
 
 # -- General configuration ---------------------------------------------------
 
