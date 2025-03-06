443fc29987e61b249270ca19e56f8809abd27219
Andre Odendaal <andre@vivosa.co.za>
2025-03-06 09:58:48 +0100

    REL-1988: Create release v0.13.0

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -6,9 +6,9 @@ copyright = '2024, SKAO'
 author = 'SKAO'
 
 # The short X.Y version
-version = '0.12.0'
+version = '0.13.0'
 # The full version, including alpha/beta/rc tags
-release = '0.12.0'
+release = '0.13.0'
 
 # -- General configuration ---------------------------------------------------
 
