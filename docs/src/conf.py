3a903c9eb60918a08d1ae9eafeb9c6a9761f2c73
Andre Odendaal <andre@vivosa.co.za>
2026-01-14 09:17:21 +0200

    REL-2626: Create release v0.15.0

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -6,9 +6,9 @@ copyright = '2024, SKAO'
 author = 'SKAO'
 
 # The short X.Y version
-version = '0.14.0'
+version = '0.15.0'
 # The full version, including alpha/beta/rc tags
-release = '0.14.0'
+release = '0.15.0'
 
 # -- General configuration ---------------------------------------------------
 
