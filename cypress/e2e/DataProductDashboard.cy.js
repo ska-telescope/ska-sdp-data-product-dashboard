70a6bc2ae931bdec64f97236c4fde7487184acff
Jacob Maresca <jacob.maresca@gmail.com>
2026-03-31 13:57:54 +0100

    PHX-369 tests

--- cypress/e2e/DataProductDashboard.cy.js
+++ cypress/e2e/DataProductDashboard.cy.js
@@ -53,7 +53,7 @@ context('Select and download data product', () => {
     })
 
     it('Verify form is filled correct', () => {
-      // cy.findAllByTestId('textEntry-Key').should('contain', 'execution_block').should('be.visible');
+      cy.findAllByTestId('textEntry-Key').should('contain', 'execution_block').should('be.visible');
       cy.findAllByTestId('textEntry-Value').find('input').should('have.value', 'eb-test-20260101-1234').should('be.visible');
     })
   })
