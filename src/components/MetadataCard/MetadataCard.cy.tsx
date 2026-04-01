8b1cd908fd4f15d69943c1d209f185d9f40251e3
Nijin Thykkathu <nijin.thykkathu@stfc.ac.uk>
2026-04-01 15:13:18 +0100

    phx-421 updated the test

--- src/components/MetadataCard/MetadataCard.cy.tsx
+++ src/components/MetadataCard/MetadataCard.cy.tsx
@@ -2,21 +2,28 @@ import React from 'react';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { THEME_DARK, THEME_LIGHT } from '@ska-telescope/ska-gui-components';
 import theme from '@services/theme/theme';
-import mockData from '@services/Mocking/mockMetaData';
 import MetadataCard from '@components/MetadataCard/MetadataCard';
 
 const THEME = [THEME_DARK, THEME_LIGHT];
 
+const emptyDataProduct = {
+  execution_block: '',
+  relativePathName: '',
+  metaDataFile: '',
+  uid: '',
+  metadata_store_name: ''
+};
+
 describe('<MetadataCard />', () => {
   for (const theTheme of THEME) {
-    it('Theme ' + theTheme + ': Renders', () => {
+    it('Theme ' + theTheme + ': Renders placeholder when no data product is selected', () => {
       cy.mount(
         <ThemeProvider theme={theme(theTheme)}>
           <CssBaseline />
-          <MetadataCard metaData={mockData} />
+          <MetadataCard {...emptyDataProduct} />
         </ThemeProvider>
       );
-      cy.findByTestId('metadata-section-context').should('be.visible');
+      cy.contains('Select a data product to view its metadata').should('be.visible');
     });
   }
 });
